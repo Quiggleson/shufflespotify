38f5be8b946f1ad228b1917eda9413bdd71fdb71
Robert Quigley <robertquigley@oakland.edu>
2024-10-31 19:26:05 -0400

    fix root path

--- app/topnav.tsx
+++ app/topnav.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 const basePath = process.env.NEXT_PUBLIC_ROOT_PATH;
 
 export default function TopNav() {
@@ -5,6 +7,7 @@ export default function TopNav() {
         <div className="h-16 m-2 flex">
             <a href={basePath + "/"} className="outline rounded px-2 py-1 mx-2 h-fit">Home</a>
             <a href={basePath + "/login"} className="outline rounded px-2 py-1 mx-2 h-fit">Login</a>
+            <p>basepath: {basePath}</p>
         </div>
     );
 }
