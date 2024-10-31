4c3870528184ae18405467543a40e4b825123130
Robert Quigley <robertquigley@oakland.edu>
2024-10-31 19:39:27 -0400

    remove basepath debug info

--- app/topnav.tsx
+++ app/topnav.tsx
@@ -8,7 +8,6 @@ export default function TopNav() {
         <div className="h-16 m-2 flex">
             <a href={basePath + "/"} className="outline rounded px-2 py-1 mx-2 h-fit">Home</a>
             <a href={basePath + "/login"} className="outline rounded px-2 py-1 mx-2 h-fit">Login</a>
-            <p>basepath: {basePath}</p>
         </div>
     );
 }
