9f198f0c55ff6835d297eb03353bde9ad90e837a
Robert Quigley <robertquigley@oakland.edu>
2024-10-31 19:33:04 -0400

    remove gitignore effect on prod env

--- app/topnav.tsx
+++ app/topnav.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-const basePath = process.env.NEXT_PUBLIC_ROOT_PATH;
-
 export default function TopNav() {
+
+    const basePath = process.env.NEXT_PUBLIC_ROOT_PATH;
+    
     return (
         <div className="h-16 m-2 flex">
             <a href={basePath + "/"} className="outline rounded px-2 py-1 mx-2 h-fit">Home</a>
